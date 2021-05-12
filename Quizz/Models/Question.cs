6fc1e1a4810bfdd7b0892cc63dd235329f653f1c
lpayet <lpayet0106@gmail.com>
2021-05-12 16:23:34 +0200

    [Quizz] Début modèle

---
+++ Quizz/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizzNoGood.Models
+{
+    public class Question
+    {
+        public string Sentence { get; private set; } 
+        public string Answer { get; private set; }
+        public string False1 { get; private set; }
+        public string False2 { get; private set; }
+        public string False3 { get; private set; }
+    }
+}
---
+++ Quizz/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizzNoGood.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; private set; }
+        public string CryptedPassword { get; set; }
+        
+        public User()
+        {
+
+        }
+
+        public bool VerifyPassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
