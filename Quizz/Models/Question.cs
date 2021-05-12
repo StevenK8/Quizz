ec7872f0e76e23003996baafee269d6230dfc78b
lpayet <lpayet0106@gmail.com>
2021-05-12 16:39:29 +0200

    model

--- Quizz/Models/Question.cs
+++ Quizz/Models/Question.cs
@@ -10,7 +10,18 @@ namespace QuizzNoGood.Models
         public string Sentence { get; private set; } 
         public string Answer { get; private set; }
         public string False1 { get; private set; }
+        /// <summary>
+        /// Possibly null
+        /// </summary>
         public string False2 { get; private set; }
+        /// <summary>
+        /// Possibly null
+        /// </summary>
         public string False3 { get; private set; }
+    
+        public bool VerifyAnswer(string answer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
